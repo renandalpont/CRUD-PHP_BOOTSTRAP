066949ffd92334b50844faa7fa4c3930d4b5942a
Renan Dal Pont Boeira <renan.dalpont@outlook.com>
2021-09-19 12:43:45 -0300

    Update config.php
    /** caminho no server para o sistema **/
    if ( !defined('BASEURL') )
    	define('BASEURL', '/GItHubX/CRUD-PHP_BOOTSTRAP/');

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ if ( !defined('ABSPATH') )
 	
 /** caminho no server para o sistema **/
 if ( !defined('BASEURL') )
-	define('BASEURL', '/CRUD-PHP-BOOTSTRAP/');
+	define('BASEURL', '/GItHubX/CRUD-PHP_BOOTSTRAP/');
 	
 /** caminho do arquivo de banco de dados **/
 if ( !defined('DBAPI') )
